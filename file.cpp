4b6496af78957b9bb76e7f69aa9493a31647d7c6
Maxime ANDRE <maxime.andre@imelavi.fr>
2008-12-19 12:57:18 +0100

    correction d'erreurs de frappe

--- file.cpp
+++ file.cpp
@@ -3,13 +3,13 @@
 
 file::file(const char *path)
 {
-	this->path=new char[strlen(path)+];
+	this->path=new char[strlen(path)+1];
 	strcpy(this->path, path);
 }
 
 file::file(const file &cfile)
 {
-	this->path=new char[strlen(cfile.path)+];
+	this->path=new char[strlen(cfile.path)+1];
 	strcpy(this->path, cfile.path);
 }
 
--- tag.cpp
+++ tag.cpp
@@ -7,7 +7,7 @@ tag::tag(const char *name)
 	strcpy(this->name, name);
 }
 
-tag::tag(const tag *ctag)
+tag::tag(const tag &ctag)
 {
 	this->name=new char[strlen(ctag.name)+1];
 	strcpy(this->name, ctag.name);
